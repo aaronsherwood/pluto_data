2815ce352705d855c43f1c445bdfddc06bf9e963
aaronsherwood <aaronsherwood>
2013-09-15 03:02:39 +0200

    Initial Commit

---
+++ pluto_data_structure/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  pluto_data_structure
+//
+//  Created by Aaron Sherwood on 9/15/13.
+//  Copyright (c) 2013 Aaron Sherwood. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
